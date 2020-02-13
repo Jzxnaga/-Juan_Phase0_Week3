0e917dfccbaab3cc1f7d8a20561e43abcf6675ea
bzxnaga <bzxnaga@gmail.com>
2020-02-13 11:57:11 +0700

    last

--- 6.Logic Challenge - Palindrome.js
+++ 6.Logic Challenge - Palindrome.js
@@ -1,31 +1,40 @@
-function angkaPalindrome(num){
-    var temp =''
-    var temp1=''
-    var temp2=''
-    var status = false
-    while(status === false){
-      if(num<9){
-        return num+1
-      }else{
-        num++
-        temp= num.toString()
-        for(var i = temp.length-1; i >=0;i--){
-          temp1+= temp[i]
-        }
-        temp2 = Number(temp1)
-        if(num===temp2){
-          status = true;
-          return num
-        }else{
-          temp1=''
-          status = false;
-        }
+function angkaPalindrome(num) {
+  // var temp = num.toString();
+  var temp1 = "";
+  var temp= num.toString()
+  for (var i = temp.length - 1; i >= 0; i--) {
+    temp1 += temp[i];
+  }
+  // console.log (temp1+"initemp1")
+  // temp2 = Number(temp1);
+  // console.log(num+"ininum")
+  // console.log (temp2+"initemp2")
+  // console.log(num)
+  // console.log(temp1)
+  if (num < 9) {
+    return num + 1;
+  } else {
+    while (num != temp1) {
+      
+
+
+
+      
+      console.log (num , temp1)
+      num++
+      temp1 = ""
+      temp = num.toString()
+      for (var i = temp.length - 1; i >= 0; i--) {
+        temp1 += temp[i];
       }
     }
   }
-  // TEST CASES
-  console.log(angkaPalindrome(8)); // 9
-  console.log(angkaPalindrome(10)); // 11
-  console.log(angkaPalindrome(117)); // 121
-  console.log(angkaPalindrome(175)); // 181
-  console.log(angkaPalindrome(1000)); // 1001
+  return num
+}
+// TEST CASES
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
+console.log(angkaPalindrome(1800)); // 1001
